56fd3d83df8041952bfa006a5624d12cc577d261
fethomas <fethomas@ursinus.edu>
2018-11-30 17:11:59 -0500

    LSTM

--- matrix-pipeline-sockeye.sh
+++ matrix-pipeline-sockeye.sh
@@ -6,7 +6,7 @@ python3 -m sockeye.train --source transpose.train.source \
                        --target transpose.train.target \
                        --encoder transformer \
                        --decoder transformer \
-					   -rnn-hidden-layer lstm \
+					   --rnn-cell-type lstm \
 					   --max-num-epochs 100 \
                        --validation-source transpose.dev.source \
                        --validation-target transpose.dev.target \
