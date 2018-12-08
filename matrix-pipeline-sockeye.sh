ec07a299b9ffea51ec8d560dba65b8f995a74d0c
fethomas <fethomas@ursinus.edu>
2018-12-08 02:18:28 -0500

    epochs

--- matrix-pipeline-sockeye.sh
+++ matrix-pipeline-sockeye.sh
@@ -6,6 +6,7 @@ python3 -m sockeye.train --source transpose.train.source \
                        --target transpose.train.target \
                        --encoder transformer \
                        --decoder transformer \
+					   --rnn-cell-type lstm \
 					   --max-num-epochs 300 \
                        --validation-source transpose.dev.source \
                        --validation-target transpose.dev.target \
